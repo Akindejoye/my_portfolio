4d318a97389146672e3d5df4f566e43e7a065707
Akindejoye <tydruhill@gmail.com>
2022-11-29 23:06:03 +0100

    Build changes

--- src/components/mobileNav/MobileNav.jsx
+++ src/components/mobileNav/MobileNav.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-scroll";
 import "./mobileNav.css";
-import { FaTimes } from "react-icons/fa";
 import React from "react";
 
 const MobileNav = ({ isMobile, openNav }) => {
