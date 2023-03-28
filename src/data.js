93ca6207c4dc7b78cddb5d9fb9fe806640478032
Akindejoye <tydruhill@gmail.com>
2023-03-29 00:25:07 +0100

    Evo Gym & UUid Package

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
+import { v4 as uuidv4 } from 'uuid';
 import DarkHorse from './images/dark-horse-agency.png';
-import Commercefy from './images/commercefy.png';
+import EvoGym from './images/evo-gym.png';
 import NatureBlog from './images/nature-blog.png';
 import Menu from './images/menu.png';
 import OceanSea from './images/oceansea.png';
@@ -8,44 +9,44 @@ import UserAdmin from './images/useradmin.png';
 import Yolo from './images/yolo.png';
 
 export const products = [
+  {
+    id: uuidv4(),
+    img: Yolo,
+    link: "https://yolo-ecommerce-rosy.vercel.app/",
+  },
+  {
+    id: uuidv4(),
+    img: EvoGym,
+    link: "https://evogym-neon.vercel.app/",
+  },
     {
-      id: 1,
-      img: Yolo,
-      link: "https://yolo-ecommerce-rosy.vercel.app/",
-    },
-    {
-      id: 2,
+      id: uuidv4(),
       img: DarkHorse,
       link: "https://dark-horse-agency.netlify.app/",
     },
     {
-      id: 3,
-      img: Commercefy,
-      link: "https://commercefy.netlify.app/",
-    },
-    {
-      id: 4,
+      id: uuidv4(),
       img: NatureBlog,
       link: "https://nature-blogapp.netlify.app/",
     },
     {
-      id: 5,
+      id: uuidv4(),
       img: Menu,
       link: "https://menu-delicacies.netlify.app/",
     },
     
     {
-      id: 7,
+      id: uuidv4(),
       img: UserAdmin,
       link: "https://useradmingeneral.netlify.app/",
     },
     {
-      id: 7,
+      id: uuidv4(),
       img: OceanSea,
       link: "https://oceansea-blockchain.vercel.app/",
     },
     {
-      id: 8,
+      id: uuidv4(),
       img: Tesla,
       link: "https://tesla-classic.netlify.app/",
     },
