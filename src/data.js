b1bd5278bea8abe9202d954e0551b3c39b9f37f7
Akindejoye <tydruhill@gmail.com>
2023-09-26 12:30:12 +0100

    Maintenance

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 import { v4 as uuidv4 } from 'uuid';
 import DarkHorse from './images/dark-horse-agency.png';
 import EvoGym from './images/evo-gym.png';
-import Menu from './images/menu.png';
 import OceanSea from './images/oceansea.png';
 import Tesla from './images/tesla.png';
 import UserAdmin from './images/useradmin.png';
 import Yolo from './images/yolo.png';
 import NewsBlog from './images/news-blog.png';
 import EcomVanilla from './images/ecommerce-vanilla.png';
+import Billion from './images/billion.png';
 
 export const products = [
   {
     id: uuidv4(),
-    img: Yolo,
-    link: "https://yolo-ecommerce-rosy.vercel.app/",
+    img: NewsBlog,
+    link: "https://news-blog-iota-woad.vercel.app/",
   },
   {
     id: uuidv4(),
@@ -22,25 +22,19 @@ export const products = [
   },
   {
     id: uuidv4(),
-    img: EcomVanilla,
-    link: "https://ecommerce-vanilla-design.vercel.app/",
-  },
-  {
-    id: uuidv4(),
     img: DarkHorse,
     link: "https://dark-horse-agency.netlify.app/",
   },
   {
     id: uuidv4(),
-    img: NewsBlog,
-    link: "https://news-blog-iota-woad.vercel.app/",
+    img: Billion,
+    link: "https://billion-dashboard-client.vercel.app/dashboard",
   },
   {
     id: uuidv4(),
-    img: Menu,
-    link: "https://menu-delicacies.netlify.app/",
+    img: EcomVanilla,
+    link: "https://ecommerce-vanilla-design.vercel.app/",
   },
-  
   {
     id: uuidv4(),
     img: UserAdmin,
@@ -51,6 +45,12 @@ export const products = [
     img: OceanSea,
     link: "https://oceansea-blockchain.vercel.app/",
   },
+  {
+    id: uuidv4(),
+    img: Yolo,
+    link: "https://yolo-ecommerce-rosy.vercel.app/",
+  },
+  
   {
     id: uuidv4(),
     img: Tesla,
